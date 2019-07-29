b4be0d72ed46c8d880fc6df4e5148f4f5aa405b9
Lisa Jensen <leese_38@yahoo.com>
2019-07-29 14:12:48 -0700

    create new template

---
+++ ApplicationName/Controllers/HomeControllers.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using NamespaceName;
+
+namespace NamespaceName.Controllers
+{
+    public class HomeController : Controllers
+    {
+
+    }
+}
---
+++ ApplicationName/Models/ClassName.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace NamespaceName
+{
+    public class ClassName
+    {
+
+    }
+}
